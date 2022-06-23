f81fe64157b0031fa86196003bb5ad3b1954fb9c
Sergey Golovin <sk13man@gmail.com>
2022-06-23 22:21:02 +0300

    Jest (#4)
    * added jest workflow
    
    * video
    
    * jest-job
    
    * 123
    
    * jest
    
    * jest
    
    * jest opts
    
    * jest del
    
    * rename yaml
    
    * 123
    
    * single job
    
    * try
    
    * typo
    
    * 098
    
    * 12
    
    * fix
    
    * again
    
    * split
    
    * typo
    
    * 123
    
    * concat
    
    * structure

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
 
-  video: true,
+  video: false,
   fixturesFolder: false,
   screenshotOnRunFailure: true,
   videosFolder: './cypress/reports/videos',
